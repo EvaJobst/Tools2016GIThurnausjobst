7581365f6a8ecb12949214f42fc5cb6170241e9f
EvaJobst <S1510237014@fh-hagenberg.at>
2016-03-10 17:22:40 +0100

    Added class variable and constructor

--- hurnaus_jobst_java/src/dummyProject2/MainActivity.java
+++ hurnaus_jobst_java/src/dummyProject2/MainActivity.java
@@ -2,6 +2,11 @@ package dummyProject2;
 
 // This is our first class
 public class MainActivity {
+	private int[] array;
+	
+	public void dummyProject2(int size) {
+		int[] array = new int[size];
+	}
 
 	public void thisIsANewMehod() {
 		int x = 10 + 10;		
