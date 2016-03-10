618487f0daabf0a0491abe2c39c3acca2d2339a7
Fabian Hurnaus <fahu@outlook.com>
2016-03-10 17:02:51 +0100

    changed formatting

--- hurnaus_jobst_java/src/dummyProject2/MainActivity.java
+++ hurnaus_jobst_java/src/dummyProject2/MainActivity.java
@@ -1,12 +1,9 @@
 package dummyProject2;
 
+// This is our first class
 public class MainActivity {
-<<<<<<< HEAD
+
 	public void thisIsANewMehod() {
 		
 	}
-	
-=======
-	// This is our first class
->>>>>>> 33142197f1cbde1502f7d0fe3a10df85713a6b07
 }
