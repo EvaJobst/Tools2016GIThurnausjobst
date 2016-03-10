bb2998071ad67abc9bba824d44a12cfe3ab5c7a3
EvaJobst <S1510237014@fh-hagenberg.at>
2016-03-10 16:58:34 +0100

    Added new method

--- hurnaus_jobst_java/src/dummyProject2/MainActivity.java
+++ hurnaus_jobst_java/src/dummyProject2/MainActivity.java
@@ -1,5 +1,12 @@
 package dummyProject2;
 
 public class MainActivity {
+<<<<<<< HEAD
+	public void thisIsANewMehod() {
+		
+	}
+	
+=======
 	// This is our first class
+>>>>>>> 33142197f1cbde1502f7d0fe3a10df85713a6b07
 }
